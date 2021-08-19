126db975f152d4e97efe74eb16989104239706e7
toshmanuel <oladimejiomotosho5@gmail.com>
2021-08-19 11:18:06 +0100

    updated

--- src/main/java/com/boardroomone/profilephotoservice/config/CloudinaryConfig.java
+++ src/main/java/com/boardroomone/profilephotoservice/config/CloudinaryConfig.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.lang.invoke.DelegatingMethodHandle$Holder;
 
 @Component
 @Data
